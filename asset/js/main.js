a6b42d316e22cfef2eb5dcb26246e9b035d01eb0
luis angel ponce alvarez <poncealvarezluisangel4@gmail.com>
2023-02-17 18:26:39 -0600

    retoques

--- asset/js/main.js
+++
@@ -1,6 +0,0 @@
-/* ===== Loader =====*/
-window.addEventListener('load', () => {
-  const contenedorLoader = document.querySelector('.container--loader');
-    contenedorLoader.style.opacity = 0;
-    contenedorLoader.style.visibility = 'hidden';
-})
